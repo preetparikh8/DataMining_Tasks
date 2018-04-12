1812a800cddb4fac92789e47ac7e92ef9bd2f984
preetparikh8 <31597155+preetparikh8@users.noreply.github.com>
2018-04-11 20:05:59 -0400

    HW6

--- HW6/6A.py
+++ HW6/6A.py
@@ -7,6 +7,7 @@ import pylab as pl
 import numpy as np
 from sklearn import cross_validation
 from sklearn.grid_search import GridSearchCV
+import json
 
 stopwords = ["a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also","although","always","am","among", "amongst", "amoungst", "amount",  "an", "and", "another", "any","anyhow","anyone","anything","anyway", "anywhere", "are", "around", "as",  "at", "back","be","became", "because","become","becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom","but", "by", "call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven","else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own","part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thickv", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the", "don\\\\u2019t", "&amp;"]
 
@@ -60,4 +61,27 @@ print clf.best_score_
 print clf.best_params_
 
 
+# predict the class labels of new tweets
+print clf.predict(X)
+tweets = []
+for line in open('unlabeled_tweets.txt').readlines():
+    tweets.append(line)
+
+# Generate X for testing tweets
+X = []
+for text in tweets:
+    x = [0] * len(vocab)
+    terms = [term for term in text.split() if len(term) > 2]
+    for term in terms:
+        if vocab.has_key(term):
+            x[vocab[term]] += 1
+    X.append(x)
+y = clf.predict(X)
+
+# print 100 example tweets and their class labels
+for idx in range(1,400):
+    data = {y[idx],tweets[idx]}
+    with open("predicted_tweets.txt", 'a+') as file:
+        file.write(json.dumps(str(data)))
+        file.write("\n")
 
